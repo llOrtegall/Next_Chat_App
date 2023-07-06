13060b3d7b3d84acd7a288b0ffd2e708113cd298
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-05 19:11:08 -0500

    agregando stilos al register

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,5 +1,10 @@
 export default function Register() {
   return (
-    <div>Registrer Screen Here</div>
+    <div className="bg-blue-50 h-screen ">
+      <form className="w-64 mx-auto">
+        <input type="text" placeholder="username" className="block w-full " />
+        <input type="password" placeholder="password" className="block w-full " />
+      </form>
+    </div>
   );
 }
