d5bc9e80a69b5bcced4ddc76417b89ad82427e4a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-06 21:09:21 -0500

    probar api

--- api/index.js
+++ api/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const jwt = require('jsonwebtoken');
+const { json } = require('body-parser');
 
-dotenv.config(process.env.MONGO_URL);
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+jwtSecret = process.evt.JWT_SECRET;
+
 const app = express();
 
 app.get('/test', (req, res) => {
   res.json('test ok');
 })
 
-app.post('/register', (req, res) => {
-
-})
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const createdUser = await User.create({ username, password });
+  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie('token', token).status(201).json('ok');
+  });
+});
 
 app.listen(4040);
 
