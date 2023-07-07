c86b7954d1043ea1908ba9419b229b51f208f465
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-06 20:57:08 -0500

    AGAIN

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config(process.env.MONGO_URL);
 const app = express();
 
 app.get('/test', (req, res) => {
   res.json('test ok');
 })
 
-app.listen(4000);
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(4040);
+
+//mongodb+srv://mernchat:TmeYNGI6OXCrvl3w@cluster0.smc1eaf.mongodb.net/?retryWrites=true&w=majority
+//TmeYNGI6OXCrvl3w
