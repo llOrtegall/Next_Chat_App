a8dc8ad3894558609120100b95fa55d62615a2ec
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-19 14:31:39 -0500

    arreglado el tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 export function App () {
   return (
-    <h1>test</h1>
+    <div className='bg-blue-200 h-screen'>
+      <form className='w-64 mx-auto'>
+        <input type='text' placeholder='Usuario' className='block' />
+        <input type='password' placeholder='Contraseña' className='block' />
+        <button className='bg-blue-500 text-white block w-full p-2'>Registrarse</button>
+      </form>
+    </div>
   )
 }
