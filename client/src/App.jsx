3f516bbfc258ffaeea13ac3c819aa1f4f40619b8
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-19 14:43:42 -0500

    continuo con mi api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
+
 export function App () {
+  const [usuario, setUsuario] = useState('')
+  const [contraseña, setContraseña] = useState('')
+
   return (
-    <div className='bg-blue-200 h-screen'>
-      <form className='w-64 mx-auto'>
-        <input type='text' placeholder='Usuario' className='block' />
-        <input type='password' placeholder='Contraseña' className='block' />
-        <button className='bg-blue-500 text-white block w-full p-2'>Registrarse</button>
+    <section className='bg-blue-200 h-screen flex items-center'>
+      <form className='w-64 mx-auto mb-12'>
+        <input
+          type='text' placeholder='Usuario' value={usuario}
+          className='block w-full rounded-md p-2 mb-2 border'
+          onChange={ev => setUsuario(ev.target.value)}
+        />
+        <input
+          type='password' placeholder='Contraseña' value={contraseña}
+          className='block w-full rounded-md p-2 mb-2 border'
+          onChange={ev => setContraseña(ev.target.value)}
+        />
+        <button className='bg-blue-500 text-white block w-full p-2 rounded-md'>
+          Registrarse
+        </button>
       </form>
-    </div>
+    </section>
   )
 }
