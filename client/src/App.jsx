d8d31453ae6cfbf1b5184f355e6524defa2565ad
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-10 23:32:55 -0500

    comenzamos desde 0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,5 @@
-import { UserContextProvider } from './Auth/UserContext'
-import { Routes } from './Routes/Routes'
-import axios from 'axios'
-
 export function App () {
-  axios.defaults.baseURL = 'http://localhost:3030'
-  axios.defaults.withCredentials = true
-
   return (
-    <UserContextProvider>
-      <Routes />
-    </UserContextProvider>
+    <h1>Hello World</h1>
   )
 }
