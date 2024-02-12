e1825cd008f93c74ee0dadd1ad7e4505723822bf
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-12 12:26:08 -0500

    funciona

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,18 +2,14 @@ import { Avatar } from './Avatar'
 
 export function Contact ({ id, username, online, onClick, selected }) {
   return (
-    <div>
-      <section key={id} onClick={() => onClick(id)}
-        className={`border-b border-gray-100 flex items-center gap-2 cursor-pointer 
-          ${selected ? 'bg-blue-50' : ''}`}>
-        {selected && (
-          <div className='w-1 bg-blue-500 h-full rounded-r-md'></div>
-        )}
-        <div className='flex gap-2 items-center py-2 pl-2'>
-          <Avatar online={online} username={username} userId={id} />
-          <span className='text-gray-800'>{username}</span>
-        </div>
-      </section>
-    </div>
+    <section key={id} onClick={() => onClick(id)} className={`flex cursor-pointer ${selected ? 'bg-blue-50' : ''}`}>
+      {selected && (<div className='w-1 h-full bg-blue-500 rounded-r-md'></div>)}
+      <div className='flex gap-2 items-center py-2 pl-2'>
+        <Avatar online={online} username={username} userId={id} />
+        <span className='text-gray-800 font-semibold'>{
+          username.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+        }</span>
+      </div>
+    </section>
   )
 }
