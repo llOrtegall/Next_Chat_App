b17a42d2d2e27fe89f142732c400d97cd46b1839
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-12 15:30:48 -0500

    desde 0

--- api/Models/Messages.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose'
-
-const MessageSchema = new Schema({
-  sender: { type: Schema.Types.ObjectId, ref: 'User' },
-  recipient: { type: Schema.Types.ObjectId, ref: 'User' },
-  text: String
-}, { timestamps: true, versionKey: false })
-
-export const MessageModel = model('Message', MessageSchema)
--- api/Models/Usuario.js
+++
@@ -1,8 +0,0 @@
-import mongodb from 'mongoose'
-
-const UsuarioSchema = new mongodb.Schema({
-  username: { type: String, unique: true },
-  password: { type: String }
-}, { timestamps: true, versionKey: false })
-
-export const UserModel = mongodb.model('User', UsuarioSchema)
