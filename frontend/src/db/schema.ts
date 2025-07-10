53282526ce45ca12f827767a21ebca1290885d38
llOrtegall <IvanOrtega_97@hotmail.com>
2025-07-10 09:09:48 -0500

    again

--- frontend/src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 80 }).notNull(),
-  email: varchar({ length: 120 }).notNull().unique(),
-  username: varchar({ length: 120 })
-});
