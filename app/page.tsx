bd9c4773ad88cbf86c2f719e6db1add56edfaf5b
llOrtegall <IvanOrtega_97@hotmail.com>
2025-07-10 11:58:11 -0500

    init proyect, whit shadcn

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/ui/theme"
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle />
+      <h1>Welcome to the Next.js App</h1>
+      <p>This is a simple page to demonstrate the layout and font integration.</p>
+      <p>Enjoy building your application!</p>
+    </div>
+  );
+}
